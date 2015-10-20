070c6de1c6865591852604b33820cb4c11db6af1
DrewNicholas <nicholas.am.s@141H14.petoskeyschools.org>
2015-10-20 14:15:49 -0400

    Change die to an array

--- project4.pl
+++ project4.pl
@@ -6,11 +6,10 @@
 use 5.14.1;
 use warnings;
 
-my (@playerScore, @computerScore);
-my ($num, $continueInt, $die1, $die2,$turnScore, $playerScoreSize, $computerScoreSize, $playerCounter, $computerCounter, $pTotalScore, $cTotalScore); #numeric
+my (@playerScore, @computerScore, @die);
+my ($num, $continueInt,$turnScore, $playerScoreSize, $computerScoreSize, $playerCounter, $computerCounter, $pTotalScore, $cTotalScore); #numeric
 my ($string);
 
-use constant MAX_DIE => 6;
 use constant LOSE_SCORE => 1;
 
 sub main {
@@ -22,12 +21,13 @@ sub main {
 
 main();
 
-sub setDie1 {
-     $die1 = int(rand(MAX_DIE) + 1);
-}
-
-sub setDie2 {
-     $die2 = int(rand(MAX_DIE) + 1);
+sub rollDie {
+     use constant NUMBER_OF_DIE => 2;
+     use constant MAX_DIE => 6;
+     
+     for (my $i = 0; $i < NUMBER_OF_DIE; $i++) {
+          $die[$i] = int(rand(MAX_DIE) + 1);
+     }
 }
 
 sub setContinueInt {
@@ -48,8 +48,8 @@ sub setContinueInt {
 }
 
 sub setPlayerScore {     
-     if ($die1 == LOSE_SCORE || $die2 == LOSE_SCORE) {
-          if ($die1 == LOSE_SCORE && $die2 == LOSE_SCORE) {
+     if ($die[1] == LOSE_SCORE || $die[2] == LOSE_SCORE) {
+          if ($die[1] == LOSE_SCORE && $die[2] == LOSE_SCORE) {
                for (my $i = 0; $i < $playerScoreSize; $i++) {
                     $playerScore[$i] = 0;
                     $playerCounter = 0;
@@ -58,15 +58,15 @@ sub setPlayerScore {
                $turnScore = 0;
           }
      } else {
-          $turnScore = $die1 + $die2;
+          $turnScore = $die[1] + $die[2];
           $playerCounter++;
      }
      $playerScore[$playerCounter] = $turnScore;
 }
 
 sub setComputerScore {
-     if ($die1 == LOSE_SCORE || $die2 == LOSE_SCORE) {
-          if ($die1 == LOSE_SCORE && $die2 == LOSE_SCORE) {
+     if ($die[1] == LOSE_SCORE || $die[2] == LOSE_SCORE) {
+          if ($die[1] == LOSE_SCORE && $die[2] == LOSE_SCORE) {
                for (my $i = 0; $i < $computerScoreSize; $i++) {
                     $computerScore[$i] = 0;
                     $computerCounter = 0;
@@ -75,7 +75,7 @@ sub setComputerScore {
                $turnScore = 0;
           }
      } else {
-          $turnScore = $die1 + $die2;
+          $turnScore = $die[1] + $die[2];
           $computerCounter++;
      }
      $computerScore[$computerCounter] = $turnScore;
