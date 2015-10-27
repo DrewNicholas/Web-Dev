519a8d13053ca56d4c28779109becc0ac03f788b
DrewNicholas <nicholas.am.s@141H14.petoskeyschools.org>
2015-10-27 15:07:04 -0400

    start formatting project 5

---
+++ project5.pl
@@ -0,0 +1,23 @@
+# Assignment: Project 5
+# Author: Drew Nicholas (dmnicholas@me.com)
+# Version: 10.27.2015.01
+# Purpose: Apgar Medical Group merge data
+
+use 5.14.01;
+use warnings;
+
+my @data;
+#use constant DATAFILEIN => ";slghfks"; Put file address here
+#use constant DATAFILEOUT => "ghdljkdfljsg"; Put out file here
+
+sub main {
+     
+}
+
+main ();
+
+sub readData {
+     my $IN;
+     @data = ();
+     
+}
