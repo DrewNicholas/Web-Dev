154f15d53c347380411c30d77853382b2523e6cc
DrewNicholas <nicholas.am.s@141H14.petoskeyschools.org>
2015-10-30 15:08:53 -0400

    Work on project 5 input data and copy insertion sort

--- project5.pl
+++ project5.pl
@@ -7,8 +7,10 @@ use 5.14.01;
 use warnings;
 
 my @data;
-use constant DATAFILEIN => ";slghfks"; #Put file address here
-use constant DATAFILEOUT => "ghdljkdfljsg"; #Put out file here
+use constant DATAFILEIN1 => "./data.Chapter07.ApgarMedicalBest.txt";
+use constant DATAFILEIN2 => "./data.Chapter07.ApgarMedicalCushing.txt";
+use constant DATAFILEOUT => "./data.ApgarMedicalCombined.txt";
+use constant COLUMNS => 3;
 
 sub main {
 
@@ -18,12 +20,41 @@ main ();
 
 sub readData {
      my $IN;
-     @data = ();
-     my $counter = 0;
-     open ($IN, '<', DATAFILEIN);
-     while (<$IN>) {
-          chomp ($data[$counter] = $_);
-          $counter++
-     }
-     close $IN;
+	my $counter = 0;
+	my @tempData = ();
+	@data = ();
+	open ($IN, '<', DATAFILEIN1);
+	while (<$IN>) {
+		@tempData = split(/,/);
+		for (my $i = 0; $i < COLUMNS; $i++) {
+			chomp ($data[$counter][$i] = $tempData[$i]);
+		}
+		$counter++;
+	}
+	close $IN;
+	
+	@tempData = ();
+	open ($IN, '<', DATAFILEIN2);
+	while (<$IN>) {
+		@tempData = split (/,/);
+		for (my $i = 0; $i < COLUMNS; $i++) {
+			chomp ($data[$counter][$i] = $tempData[$i]);
+		}
+		$counter++
+	}
+	close $IN;
+}
+
+sub modifyData {
+	my $size = @data;
+	my $temp = 0;
+	for (my $i = 1; $i < $size; $i++) {
+		my $j = $i;
+		while ($j > 0 && $data[$j] < $data[$j - 1]) {
+			$temp = $data[$j];
+			$data[$j] = $data[$j - 1];
+			$data[$j - 1] = $temp;
+			$j--;
+		}
+	}
 }
