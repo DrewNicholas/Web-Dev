d66d85d3233d97886c0f0f53cadb8be1640f9c1c
DrewNicholas <nicholas.am.s@141H14.petoskeyschools.org>
2015-11-06 14:52:37 -0500

    read word bank and select a word

---
+++ project 6/project6.pl
@@ -0,0 +1,40 @@
+# Assignment: Project 6
+# Author: Drew Nicholas (dmnicholas@me.com)
+# Version: 11.04.2015.01
+# Purpose: Hangman
+
+use 5.14.01;
+use warnings;
+
+my @words;
+use constant DATAFILEIN => "./wordBank.txt";
+
+sub main {
+     print "\n     O";
+     print "\n    /|\\ ";
+     print "\n     |";
+     print "\n    / \\ ";
+     print "\n\n";
+     readData();
+     selectWord();
+}
+
+main();
+
+sub readData {
+     my $IN;
+	@words = ();
+	my $counter = 0;
+	open ($IN, '<', DATAFILEIN);
+	while (<$IN>) {
+		chomp ($words[$counter] = $_);
+		$counter++;
+	}
+	close $IN;
+}
+
+sub selectWord {
+     my $size = @words;
+     my $answer = $words[int(rand($size))];
+     print "$answer";
+}
