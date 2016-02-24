bbdc58972c1998c33a1f568ad9b17e632cec0744
T24Brown <readingmaniac24@gmail.com>
2016-02-24 15:19:43 -0500

    school emergency project

---
+++ webApps/template/scripts/Decision.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Administrator on 2/24/2016.
+ */
+
+"use strict";
+
+export default class Decision {
+    constructor() {
+
+    }
+}
---
+++ webApps/template/scripts/main.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Administrator on 2/24/2016.
+ */
+
+"use strict";
+
+import Decision from "./Decision";
+
+class main {
+    constructor() {
+
+    }
+}
+
+window.onload = function() {
+    new main();
+};
