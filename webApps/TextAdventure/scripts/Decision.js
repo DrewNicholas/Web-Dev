b8c3775e65560cfad879d3f30a9e7ed10b2475f4
Drew Nicholas <dmnicholas@me.com>
2016-03-01 15:10:59 -0500

    small work class 3/1

--- webApps/TextAdventure/scripts/Decision.js
+++ webApps/TextAdventure/scripts/Decision.js
@@ -11,12 +11,17 @@ export default class Decision {
     constructor(time, choice) {
         this.time = time;
         this.choice = choice;
-        new FileLoader().loadData('./data/instructions.csv', this.updateDOM());
+        Decision.data = "";
+        new FileLoader().loadData('./data/instructions.csv', this.setData);
     }
+
     updateDOM = function() {
         if (this.time == 'first' && this.choice == 'left') {
             document.getElementById('instructions').innerHTML = 'some other bullshit';
         }
     }
 
+    setData = function {
+
+    }
 }
