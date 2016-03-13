95402a33dfda123cd9e8622dad653eb6e0b1dbe7
Drew Nicholas <dmnicholas@me.com>
2016-03-13 16:54:18 -0400

    ATM project setup 3/13 before dinner

---
+++ webApps/ATM/scripts/main.js
@@ -0,0 +1,17 @@
+/* AUTHOR: dmnicholas@me.com
+ * VERSION: 1.0.0
+ * CREATED: 3.13.2016
+ * PURPOSE: ATM
+ */
+
+"use strict";
+
+class main {
+    constructor() {
+
+    }
+}
+
+window.onload = function() {
+    new main();
+};
